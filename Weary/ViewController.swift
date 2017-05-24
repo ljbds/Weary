e96914b592d2b3fc8cd1b07dfffde2686898c14a
koogong <809936973@qq.com>
2017-05-24 14:30:05 +0800

    Add files via upload
    The begin of weary

---
+++ Weary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weary
+//
+//  Created by 李家豪 on 2017/5/24.
+//  Copyright © 2017年 koogong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
