492c5c611f5c1ebf401cd3f8dbd96c1c1396c71b
koogong <809936973@qq.com>
2017-05-25 11:40:05 +0800

    测试

--- Weary/ViewController.swift
+++ Weary/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Hello World")
+        print("Hello koogong")
     }
 
     override func didReceiveMemoryWarning() {
